5db7d04a27841ac97fc9e7084eeae2a432cf92aa
Hier x <wel.badane1235@gmail.com>
2026-05-23 09:30:53 +0300

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME='hirex-v1';
-const ASSETS=['./','./index.html','./candidate.html','./company.html'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
