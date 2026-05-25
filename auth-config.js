53f6539f325a64d24b62f4408dbf1e1138ae7ae1
Hier x <wel.badane1235@gmail.com>
2026-05-25 13:24:38 +0300

    Add files via upload

---
+++ auth-config.js
@@ -0,0 +1,6 @@
+// HireX Professional Supabase Configuration
+// ملاحظة: Supabase JS يحتاج رابط المشروع الأساسي بدون /rest/v1
+window.HIREX_SUPABASE_URL = "https://odfcsexotylhsstybiju.supabase.co";
+window.HIREX_SUPABASE_REST_URL = "https://odfcsexotylhsstybiju.supabase.co/rest/v1";
+window.HIREX_SUPABASE_ANON_KEY = "sb_publishable_zJnpojEcE7O9kOfrnIt4Iw_9UGzunBH";
+window.HIREX_AUTH_REDIRECT_URL = window.location.origin + window.location.pathname;
