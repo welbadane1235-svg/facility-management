e3da317f2bc63557c085007bc31597ba4ccde726
Hier x <wel.badane1235@gmail.com>
2026-05-25 11:37:51 +0300

    Add files via upload

---
+++ auth-config.js
@@ -0,0 +1,4 @@
+// HireX Auth Configuration
+window.HIREX_SUPABASE_URL = "ضع_رابط_مشروع_Supabase_هنا";
+window.HIREX_SUPABASE_ANON_KEY = "ضع_anon_public_key_هنا";
+window.HIREX_AUTH_REDIRECT_URL = window.location.origin + window.location.pathname;
