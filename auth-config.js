394eee95a259197a32ec9b70c809af6db8bfd4a3
Hier x <wel.badane1235@gmail.com>
2026-07-07 11:29:34 +0300

    Delete auth-config.js

--- auth-config.js
+++
@@ -1,4 +0,0 @@
-// HireX Auth Configuration
-window.HIREX_SUPABASE_URL = "https://odfcsexotylhsstybiju.supabase.co";
-window.HIREX_SUPABASE_ANON_KEY = "sb_publishable_zJnpojEcE7O9kOfrnIt4Iw_9UGzunBH";
-window.HIREX_AUTH_REDIRECT_URL = window.location.origin + window.location.pathname;
